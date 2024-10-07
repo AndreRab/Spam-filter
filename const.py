02288bba91f28e7238832c2304bda5e02b50626a
AndreRab <andr.max2003@gmail.com>
2024-10-07 19:47:35 +0200

    Text input added

---
+++ const.py
@@ -0,0 +1,14 @@
+text_color = (0, 0, 0)
+bg_color = (255, 255, 255)
+input_box_color = (200, 200, 200)
+
+input_box_width = 650
+width_eps = 50
+input_box_height = 50
+max_line_length = 35 
+visible_lines_count = 9
+
+display_size = (800, 600)
+
+title_text = ['Write or copy-paste your email for check', 'for moving down use arrows']
+title_text_coordinates = [(70, 10), (170, 50)]
